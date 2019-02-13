d707be5cbd63654db684555b6e9e62707e942324
Ustimenko Alexander <ustimenko.alexander@gmail.com>
2019-02-14 00:24:58 +0700

    Add menu

--- template/html.php
+++ template/html.php
@@ -10,6 +10,7 @@
         <div id="description"><?php echo $term->description(); ?></div>
     </div>
     <object id="diagram" type="image/svg+xml" data="svg/<?php echo $svgName; ?>?<?php echo $generateDate; ?>"></object>
+    <a class="material-icons menu-icon" href="about.html">menu</a>
     <script src="static/html.js?<?php echo $generateDate; ?>"></script>
     <noscript><div><img src="https://mc.yandex.ru/watch/52092633" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 </body>
